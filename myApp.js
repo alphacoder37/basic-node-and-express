c2b2a3a6b9e2c0dfd45cfaf38a79f4a8bd64619d
Leon Cummins <leon.cummins@gmail.com>
2021-08-01 09:17:40 +1000

    first commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
+// console.log('Hello World')
 
+//  module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+console.log('Hello World')
