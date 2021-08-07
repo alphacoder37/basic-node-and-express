62321cacbd906ba5885f93afdfe2d868cba9fff6
Leon Cummins <leon.cummins@gmail.com>
2021-08-07 10:21:39 +1000

    env commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
-console.log('Hello World')
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  }
+  )
+
+  console.log(__dirname)
 
 
- module.exports = app;
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/json', function(req, res) {
+  if (process.env.MESSAGE_STYLE=uppercase) {
+  res.json({"message": "HELLO JSON"})
+  }
+  else {
+    res.json({"message": "Hello JSON"})
+
+  }
+
+})
+
+module.exports = app;
 
