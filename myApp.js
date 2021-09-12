fb38aa5da5e27e852f5f1aaeb1ef9fb783fb560d
Leon Cummins <leon.cummins@gmail.com>
2021-09-12 17:19:12 +1000

    comit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
+var bodyParser = require('body-parser')
+
 
 
 app.get('/', function(req, res) {
@@ -7,21 +10,39 @@ app.get('/', function(req, res) {
   }
   )
 
-  console.log(__dirname)
-
-
+app.get('/', function(req, res, next){
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  console.log(req.ip)
+  next()
+})
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/json', function(req, res) {
-  if (process.env.MESSAGE_STYLE=uppercase) {
+  if (process.env.MESSAGE_STYLE='uppercase') {
   res.json({"message": "HELLO JSON"})
   }
   else {
     res.json({"message": "Hello JSON"})
-
   }
-
 })
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString()
+  next()
+}, function(req, res)
+{
+res.json({"time": req.time})
+})
+
+app.get('/:word/echo', function(req, res, next) {
+  res.json({'echo': req.params.word})
+})
+
+app.get('/name', function(req, res, next) {
+  res.json({'name': `${req.query.firstname} ${req.query.lastname}`})
+})
+
+console.log(bodyParser)
 
 module.exports = app;
 
